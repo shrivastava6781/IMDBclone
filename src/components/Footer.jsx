b2a3463bd6ce52dbf49f84214e9dec2001e28f97
shrivastava6781 <adityashrivastava6781@gmail.com>
2023-12-16 15:10:50 +0530

    upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <div className="socials">
+            <a href="https://www.instagram.com/_a_d_i_t_y_a_97_/"><img className='media' width="24" height="24" src="https://img.icons8.com/external-tanah-basah-basic-outline-tanah-basah/24/FFFFFF/external-instagram-social-media-tanah-basah-basic-outline-tanah-basah.png" alt="external-instagram-social-media-tanah-basah-basic-outline-tanah-basah"/></a>
+            <a href="https://www.facebook.com/aditaya.shrivastava.5/"><img className='media' width="24" height="24" src="https://img.icons8.com/external-tanah-basah-glyph-tanah-basah/48/FFFFFF/external-facebook-social-media-tanah-basah-glyph-tanah-basah.png" alt="external-facebook-social-media-tanah-basah-glyph-tanah-basah"/></a>
+            <a href="https://youtube.com/@adityashrivastava970?si=WpXNQkebhgJ6aurh"><img className='media' width="24" height="24" src="https://img.icons8.com/external-tal-revivo-bold-tal-revivo/24/FFFFFF/external-youtube-is-an-american-video-sharing-and-now-googles-subsidiaries-logo-bold-tal-revivo.png" alt="external-youtube-is-an-american-video-sharing-and-now-googles-subsidiaries-logo-bold-tal-revivo"/></a>
+        
+        
+        </div>
+      <h6 className='footer__heading'>© 1990-2023 by IMDb.com, Inc.</h6>
+    </div>
+  )
+}
+
+export default Footer
